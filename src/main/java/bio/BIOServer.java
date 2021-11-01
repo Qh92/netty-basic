f42b8a874e2784658bcf6272eb4668eb041917b2
Qh92 <771600967@qq.com>
2021-11-01 21:52:18 +0800

    feat:提交代码

--- src/main/java/bio/BIOServer.java
+++ src/main/java/bio/BIOServer.java
@@ -1,6 +1,5 @@
 package bio;
 
-import java.io.IOException;
 import java.io.InputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -10,26 +9,48 @@ import java.util.concurrent.Executors;
 /**
  * @author Qh
  * @version 1.0
- * @date 2021-10-31 21:50
+ * @date 2021-11-01 21:50
  */
 public class BIOServer {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
+
+        //线程池机制
+
+        //思路
+        //1. 创建一个线程池
+        //2. 如果有客户端连接，就创建一个线程，与之通讯(单独写一个方法)
+
         ExecutorService newCachedThreadPool = Executors.newCachedThreadPool();
 
+        //创建ServerSocket
         ServerSocket serverSocket = new ServerSocket(6666);
 
-        System.out.println("服务器启动");
+        System.out.println("服务器启动了");
 
-        while (true){
-            System.out.println("服务器等待连接...");
-            //阻塞
-            Socket socket = serverSocket.accept();
+        while (true) {
+
+            System.out.println("线程信息 id =" + Thread.currentThread().getId() + " 名字=" + Thread.currentThread().getName());
+            //监听，等待客户端连接
+            System.out.println("等待连接....");
+            final Socket socket = serverSocket.accept();
             System.out.println("连接到一个客户端");
-            newCachedThreadPool.execute(() -> handler(socket));
+
+            //就创建一个线程，与之通讯(单独写一个方法)
+            newCachedThreadPool.execute(new Runnable() {
+                @Override
+                public void run() { //我们重写
+                    //可以和客户端通讯
+                    handler(socket);
+                }
+            });
+
         }
+
+
     }
 
-    private static void handler(Socket socket) {
+    //编写一个handler方法，和客户端通讯
+    public static void handler(Socket socket) {
 
         try {
             System.out.println("线程信息 id =" + Thread.currentThread().getId() + " 名字=" + Thread.currentThread().getName());
@@ -43,16 +64,16 @@ public class BIOServer {
                 System.out.println("线程信息 id =" + Thread.currentThread().getId() + " 名字=" + Thread.currentThread().getName());
 
                 System.out.println("read....");
-                System.out.println("数据获取....");
-                int read;
-                //read 阻塞
-                if((read = inputStream.read(bytes)) != -1) {
-                    //输出客户端发送的数据
-                    System.out.println(new String(bytes, 0, read));
+                int read =  inputStream.read(bytes);
+                if(read != -1) {
+                    System.out.println(new String(bytes, 0, read
+                    )); //输出客户端发送的数据
                 } else {
                     break;
                 }
             }
+
+
         }catch (Exception e) {
             e.printStackTrace();
         }finally {
